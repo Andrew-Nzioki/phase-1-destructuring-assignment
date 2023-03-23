a51a936b758de18a5eb94d20fbe6147e622a8fe6
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-03-23 15:28:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -29,31 +29,15 @@ const nestedMuppet = {
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
 
-const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ");
+
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-const [bessie, , dolly, babe, little] = [
-  "cow",
-  "horse",
-  "sheep",
-  "pig",
-  "chicken",
-];
+
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-const { color: blackAndWhite } = { color: "cow" };
-const { color: black } = { color: "sheep" };
-const { color: pink } = { color: "pig" };
+
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
-const [red, orange, yellow, green, blue, indigo, violet] = [
-  "red",
-  "orange",
-  "yellow",
-  "green",
-  "blue",
-  "indigo",
-  "violet",
-];
+
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
@@ -64,8 +48,15 @@ const [red, orange, yellow, green, blue, indigo, violet] = [
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 
-
-
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite = 'cow', , black = 'sheep', pink = 'pig'] = farmAnimals.split(' ');
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, indg, v] = colors;
+const [, , , , , indigo2] = colors;
 const { muppetName, color, song, job, partner } = muppet;
 const { album: { theMuppetMovie: { song2, song4 } }, nestedJob, nestedPartner } = nestedMuppet;
+
+const [, , , , , indg1 = indigo, ] = colors;
+
 
